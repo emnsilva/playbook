2918685d66c4b3cef032c5a85190e3be7d8a0372
Emerson <39090553+emnsilva@users.noreply.github.com>
2024-05-05 17:43:20 -0300

    Update install_paranaue_windows.ps1

--- #1: Vagrant Wars/install_paranaue_windows.ps1
+++ #1: Vagrant Wars/install_paranaue_windows.ps1
@@ -1,13 +1,13 @@
-# Instala o Hyper-V
-Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V -All -Online -NoRestart
+# Instala o Hyper-V no Windows 10/11
+# Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V -All -Online -NoRestart
+
+# Instala o Hyper-V no Windows Server
+Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -NoRestart
 
 # Instala o Chocolatey e os pacotes necessários
 Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y vscode git python vagrant
-
-# Muda a pasta de configuração global do Vagrant. Substitua o STORAGE_LOCATION pelo local onde a pasta deve ficar.
-setx VAGRANT_HOME “E:/.vagrant.d/”
 
 # Reinicia o sistema
 Restart-Computer -Force
