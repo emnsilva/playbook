016680a8139c49c7d0040948982a071c39ecb80a
Emerson <39090553+emnsilva@users.noreply.github.com>
2024-05-28 23:02:15 -0300

    Add files via upload

---
+++ #2 The Automation Games/install_ad.ps1
@@ -0,0 +1,16 @@
+﻿# Caminho para o arquivo JSON
+$jsonFilePath = "C:\password.json"
+
+# Ler o conteúdo do arquivo JSON
+$jsonContent = Get-Content -Path $jsonFilePath -Raw | ConvertFrom-Json
+
+# Extrair a senha do JSON e convertê-la para SecureString
+$safeModeAdminPassword = $jsonContent.SafeModeAdministratorPassword | ConvertTo-SecureString -AsPlainText -Force
+
+# Executar o script com a senha do JSON
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+Install-WindowsFeature RSAT-AD-PowerShell
+Install-WindowsFeature RSAT-ADDS
+Import-Module ADDSDeployment
+
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "WinThreshold" -DomainName "paranaue.com" -DomainNetbiosName "PARANAUE" -ForestMode "WinThreshold" -SafeModeAdministratorPassword $safeModeAdminPassword -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true
