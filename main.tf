4cb8bc0349e466cfd971119beb4d62108f4a1af3
Emerson <emnsilva@hotmail.com>
2025-07-21 16:43:17 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"  # Use a versão mais recente conforme necessário
+      version = "~> 5.0"
     }
   }
 }
 
-# Declaração das variáveis
 variable "TFC_AWS_RUN_ROLE_ARN" {
   description = "ARN da role IAM para autenticação OIDC"
   type        = string
@@ -19,26 +18,24 @@ variable "TFC_AWS_PROVIDER_AUTH" {
   default     = true
 }
 
-# Configuração do provedor AWS
 provider "aws" {
   region = "sa-east-1"  # Substitua pela sua região AWS
 
-  # Configuração para assumir uma role usando OIDC
   assume_role {
     role_arn = var.TFC_AWS_RUN_ROLE_ARN
-    # Se necessário, você pode especificar uma sessão ou uma política externa aqui
-    # session_name = "terraform-session"
-    # external_id   = "seu_external_id"
   }
 }
 
-# Exemplo de recurso: Criação de um bucket S3
 resource "aws_s3_bucket" "meu_bucket" {
   bucket = "meu-bucket-2025"
-  acl    = "private"
 
   tags = {
     Name        = "MeuBucket"
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_acl" "meu_bucket_acl" {
+  bucket = aws_s3_bucket.meu_bucket.id
+  acl    = "private"
 }
