3bb7ccabb51db3611aa50b5b25939313fbb6a2c2
Emerson <emnsilva@hotmail.com>
2025-07-22 11:22:11 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,23 +7,12 @@ terraform {
   }
 }
 
-# Declaração da variável necessária
-variable "TFC_AWS_RUN_ROLE_ARN" {
-  description = "The ARN of the AWS IAM Role to assume via OIDC"
-  type        = string
-}
-
 provider "aws" {
   region = "sa-east-1"
-  
-  assume_role_with_web_identity {
-    role_arn                = var.TFC_AWS_RUN_ROLE_ARN
-    web_identity_token_file = "/tmp/tfc_jwt.jwt"
-    session_name            = "terraform-session"
-  }
+  # Não precisa de nenhuma configuração extra - pega credenciais das variáveis env automaticamente
 }
 
 resource "aws_s3_bucket" "meu_bucket" {
-  bucket        = "meu-bucket-2025"
+  bucket        = "meu-bucket-2025-unico" # Adicione algo único
   force_destroy = true
 }
