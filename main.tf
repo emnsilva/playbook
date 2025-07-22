4beb4e2fb3cdbbaa5172b8465c297b15a27086a3
Emerson <emnsilva@hotmail.com>
2025-07-22 10:52:22 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,35 +7,17 @@ terraform {
   }
 }
 
-variable "TFC_AWS_RUN_ROLE_ARN" {
-  description = "ARN da role IAM para autenticação OIDC"
-  type        = string
-}
-
-variable "TFC_AWS_PROVIDER_AUTH" {
-  description = "Flag para habilitar autenticação OIDC no provedor AWS"
-  type        = bool
-  default     = true
-}
-
 provider "aws" {
-  region = "sa-east-1"  # Substitua pela sua região AWS
-
-  assume_role {
-    role_arn = var.TFC_AWS_RUN_ROLE_ARN
+  region = "sa-east-1"
+  
+  assume_role_with_web_identity {
+    role_arn                = var.TFC_AWS_RUN_ROLE_ARN
+    web_identity_token_file = "/tmp/tfc_jwt.jwt"
+    session_name            = "terraform-session"
   }
 }
 
 resource "aws_s3_bucket" "meu_bucket" {
-  bucket = "meu-bucket-2025"
-
-  tags = {
-    Name        = "MeuBucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket_acl" "meu_bucket_acl" {
-  bucket = aws_s3_bucket.meu_bucket.id
-  acl    = "private"
+  bucket        = "meu-bucket-2025"
+  force_destroy = true
 }
