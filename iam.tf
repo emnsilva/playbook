38c70ca7c3a930682df86980112fc3852c847446
Emerson <emnsilva@hotmail.com>
2025-07-23 12:21:33 -0300

    Create iam.tf

---
+++ iam.tf
@@ -0,0 +1,9 @@
+resource "aws_iam_openid_connect_provider" "this" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+  thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
+}
